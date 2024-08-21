1e27cec8a324c9b5a5441573870e54145aa6c6f5
angellusj <angela.souza@alunos.ufersa.edu.br>
2024-08-21 16:02:56 -0300

    corrigido

--- Unidade2/abstratas/pagamento/DemoPagamento.java
+++ Unidade2/abstratas/pagamento/DemoPagamento.java
@@ -4,8 +4,8 @@ public class DemoPagamento {
     public static void main(String[] args) {
         Fatura fatura = new Fatura(2.5, 4);
         EmpregadoAssalariado assalariado = new EmpregadoAssalariado(2.500, "Mario", "sdh25615");
-        imprimeValorPagamento(fatura);
-        imprimeValorPagamento(assalariado);
+        imprimirPagamento(fatura);
+        imprimirPagamento(assalariado);
     }
 
     public static void imprimirPagamento(Pagavel pagavel) {
